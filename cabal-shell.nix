d55a147d196dd0151f7d3fcd2e47458b1aba632e
lazyLambda <galen.sprout@gmail.com>
2022-12-19 06:28:32 +0000

    adding files for new nix setup

---
+++ cabal-shell.nix
@@ -0,0 +1,16 @@
+{ nixpkgs ? import <nixpkgs> {}
+, compiler ? "default"
+, doBenchmark ? false }:
+
+let
+  inherit (nixpkgs) pkgs;
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+  matplotlib = import ./default2.nix;
+  drv = variant (haskellPackages.callPackage matplotlib {});
+
+in
+  if pkgs.lib.inNixShell then drv.env else drv
